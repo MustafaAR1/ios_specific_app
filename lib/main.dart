4fa3114939f21348da48b0f22f28b7aa101edb8d
mustafa <mustafa@int.sybrid.com>
2022-12-10 15:18:45 +0500

    example created

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const CupertinoApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: const Text('SparkTech Solutions'),
+        trailing: CupertinoButton(
+            padding: const EdgeInsets.all(0),
+            child: const Icon(CupertinoIcons.search),
+            onPressed: () {}),
+      ),
+      child: const Center(
+        child: Text('Hello IOS User 🖐'),
+      ),
+    );
+  }
+}
